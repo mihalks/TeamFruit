a8d3734ab74e1ba1ca769b9279978b062c44eaa5
RusanovKonstantin <KR369501@mail.ru>
2020-03-05 20:54:03 +0300

    project_0.05_05032020)

---
+++ studentOrganizer/Parser.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Collections.Specialized;
+
+
+namespace studentOrganizer
+{
+    
+    public static class Parser
+    {
+        
+
+
+    }
+}
--- studentOrganizer/Program.cs
+++ studentOrganizer/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Collections.Specialized;
 
 namespace studentOrganizer
 {
@@ -14,6 +16,7 @@ namespace studentOrganizer
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
