b7536ad44a7b841f7d98ebbe32fbed3b17b2e892
RusanovKonstantin <KR369501@mail.ru>
2020-03-06 15:56:23 +0300

    pars_06032020

--- studentOrganizer/Parser.cs
+++ studentOrganizer/Parser.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Net;
 using System.Collections.Specialized;
+using System.IO;
 
 
 namespace studentOrganizer
@@ -15,7 +16,31 @@ namespace studentOrganizer
     
     public static class Parser
     {
-        
+        public static string Pars()
+        {
+            
+         if (System.IO.File.Exists("./TimeTabel.json"))
+            {
+                Console.WriteLine("true");
+
+            }
+            else 
+            {
+                 Console.WriteLine("False");
+                 
+                 //Здесь надо прописать запросы
+                 
+                using (FileStream fstream = new FileStream("./TimeTabel.json", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes("РАСПИСАНИЕ");//передать переменую полученую из запроса
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+            }
+            return "";// подумать что должно возвращаться
+        }
 
 
     }
