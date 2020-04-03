c28e8f284af2fc6d7fb5781b936554f8ec62d9bc
mihalks <jurawlew.mih@yandex.ru>
2020-04-03 15:54:45 +0300

    ne govoru gop

--- studentOrganizer/ClientApp/src/app/chat/chat.component.spec.ts
+++ studentOrganizer/ClientApp/src/app/chat/chat.component.spec.ts
@@ -1,27 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CounterComponent } from './counter.component';
+import { chatComponent } from './chat.component';
 
-describe('CounterComponent', () => {
-  let component: CounterComponent;
-  let fixture: ComponentFixture<CounterComponent>;
+describe('chatComponent', () => {
+  let component: chatComponent;
+  let fixture: ComponentFixture<chatComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
+      declarations: [ chatComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CounterComponent);
+    fixture = TestBed.createComponent(chatComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should display a title', async(() => {
     const titleText = fixture.nativeElement.querySelector('h1').textContent;
-    expect(titleText).toEqual('Counter');
+    expect(titleText).toEqual('chat');
   }));
 
   it('should start with count 0, then increments by 1 when clicked', async(() => {
