038902a6626443773c60c5c7fdec5b1f49f92607
mihalks <jurawlew.mih@yandex.ru>
2020-04-10 21:47:49 +0300

    miniminichange

---
+++ studentOrganizer/OrginizerForecast.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace studentOrganizer
+{
+    public class OrganizerForecast
+    {
+        public DateTime Date { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
