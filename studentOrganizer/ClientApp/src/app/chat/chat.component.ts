372f1307c27556401f813f5ee89f4b07ad11eff8
Eugeny Konstantinov <eugeny.konstantinov@rp.ru>
2020-04-18 13:34:12 +0300

    Added sample chat on signal-r

--- studentOrganizer/ClientApp/src/app/chat/chat.component.ts
+++ studentOrganizer/ClientApp/src/app/chat/chat.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-chat-component',
   templateUrl: './chat.component.html'
 })
-export class chatComponent {
+export class chatComponent implements OnInit {
   public currentCount = 0;
+
+  private connection;
+  name: string = '';
 
   public incrementchat() {
     this.currentCount++;
+  }
+
+  sendMessage() {
+    this.connection.invoke('Send', this.name);
+  }
+
+  async ngOnInit() {
+    this.connection = new HubConnectionBuilder().withUrl('/mychat').build();
+
+    this.connection.on('Send', (data) => {
+      console.log(data);
+    });
+    await this.connection.start();
+    this.connection.invoke('Send', 'Hello');
   }
 }
