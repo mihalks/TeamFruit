afc25418474ec6de907d41a09a5f29da0cc9ac0d
QinamruG <tyukalov.ko3700@gmail.com>
2020-08-20 20:14:11 +0300

    add timetable controller

---
+++ studentOrganizer/Timetable.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace studentOrganizer
+{
+    public static class Timetable
+    {
+        public static object GetTimetable(string group)
+        {
+            var correctGr = Regex.Replace(group, "/", "-"); // желательно, чтобы сразу нормально отправлялось
+            using (var sr = new StreamReader("TimeTabel.json", Encoding.UTF8))
+            {
+                var jsonStr = sr.ReadToEnd();
+                var fullTimetable = JsonConvert.DeserializeObject<FullTimeTable>(jsonStr);
+                var gr = fullTimetable.Faculties.First(x => x.Groups.Any(x => x.Name == correctGr)).Groups.First(x => x.Name == correctGr);
+                return gr;
+            }
+        }
+    }
+}
