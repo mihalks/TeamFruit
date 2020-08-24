798db3ee5ec48eabea6f7bbc363b00454b2753b2
QinamruG <tyukalov.ko3700@gmail.com>
2020-08-25 01:25:22 +0300

    quick fix

--- studentOrganizer/ClientApp/src/app/rating/rating.component.ts
+++ studentOrganizer/ClientApp/src/app/rating/rating.component.ts
@@ -2,8 +2,8 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-raiting',
-  templateUrl: './raiting.component.html',
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
   styleUrls: ['./style.css']
 })
 export class ratingComponent {
