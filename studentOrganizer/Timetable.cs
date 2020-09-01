4c218bbf29bafbab6201f15fa9a62b6577d91d84
QinamruG <tyukalov.ko3700@gmail.com>
2020-09-01 21:54:08 +0300

    add timetableApi from isuct site

--- studentOrganizer/Timetable.cs
+++ studentOrganizer/Timetable.cs
@@ -16,9 +16,16 @@ namespace studentOrganizer
         public static object GetTimetable(string group)
         {
             var correctGr = Regex.Replace(group, "/", "-"); // желательно, чтобы сразу нормально отправлялось
-            using (var sr = new StreamReader("TimetableApi.json", Encoding.UTF8))
+            /*using (var sr = new StreamReader("TimetableApi.json", Encoding.UTF8)) // старый код для работы через файл
             {
                 var jsonStr = sr.ReadToEnd();
+                var fullTimetable = JsonConvert.DeserializeObject<FullTimeTable>(jsonStr);
+                var gr = fullTimetable.Faculties.First(x => x.Groups.Any(x => x.Name == correctGr)).Groups.First(x => x.Name == correctGr);
+                return gr;
+            }*/
+            using (var http = new HttpClient())
+            {
+                var jsonStr = http.GetStringAsync("https://forms.isuct.ru/timetable/rvuzov").Result;
                 var fullTimetable = JsonConvert.DeserializeObject<FullTimeTable>(jsonStr);
                 var gr = fullTimetable.Faculties.First(x => x.Groups.Any(x => x.Name == correctGr)).Groups.First(x => x.Name == correctGr);
                 return gr;
